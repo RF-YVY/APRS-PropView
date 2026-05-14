9717303cb9d729666786ef649ee0ec15543ab979
RF-YVY <kd5yvy@gmail.com>
2026-05-14 11:14:31 -0500

    Add files via upload

---
+++ tests/test_aprs_compliance.py
@@ -0,0 +1,61 @@
+import unittest
+
+from server.aprs_is import APRSISClient
+from server.aprs_parser import parse_packet
+from server.config import Config
+
+
+class APRSParserComplianceTests(unittest.TestCase):
+    def test_third_party_message_is_unwrapped(self):
+        packet = parse_packet(
+            "G9RXG>APRS:}WB4APR-14>APRS,TCPIP,G9RXG*::N0CALL   :Hi{001",
+            source="rf",
+        )
+
+        self.assertEqual(packet.packet_type, "message")
+        self.assertEqual(packet.from_call, "WB4APR-14")
+        self.assertEqual(packet.to_call, "APRS")
+        self.assertEqual(packet.path, "TCPIP,G9RXG*")
+        self.assertEqual(packet.addressee, "N0CALL")
+        self.assertEqual(packet.message_text, "Hi")
+        self.assertEqual(packet.message_id, "001")
+
+    def test_compressed_position_keeps_full_comment(self):
+        packet = parse_packet("CALL>APRS:!/5L!!<*e7Pxyzcomment", source="rf")
+
+        self.assertEqual(packet.packet_type, "position")
+        self.assertAlmostEqual(packet.latitude, 49.5, places=3)
+        self.assertAlmostEqual(packet.longitude, -72.75, places=3)
+        self.assertEqual(packet.comment, "comment")
+
+    def test_compressed_position_accepts_overlay_table_id(self):
+        packet = parse_packet("CALL>APRS:!A5L!!<*e7Pxyzoverlay", source="rf")
+
+        self.assertEqual(packet.packet_type, "position")
+        self.assertEqual(packet.symbol_table, "A")
+        self.assertEqual(packet.comment, "overlay")
+
+    def test_uncompressed_course_speed_extension(self):
+        packet = parse_packet(
+            "CALL>APRS:=4903.50N/07201.75W>088/036Moving",
+            source="rf",
+        )
+
+        self.assertEqual(packet.packet_type, "position")
+        self.assertEqual(packet.course, 88)
+        self.assertAlmostEqual(packet.speed, 66.672, places=3)
+        self.assertEqual(packet.comment, "Moving")
+
+
+class APRSISComplianceTests(unittest.TestCase):
+    def test_login_uses_current_app_version(self):
+        config = Config()
+        config.station.callsign = "K5ABC"
+        config.aprs_is.passcode = "12345"
+        client = APRSISClient(config, lambda packet: None, app_version="1.3.4")
+
+        self.assertIn("vers APRSPropView 1.3.4", client._build_login())
+
+
+if __name__ == "__main__":
+    unittest.main()
